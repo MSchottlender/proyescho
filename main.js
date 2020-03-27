355b16b25099d64f2a9101a882c1fa45f47221bf
MSchottlender <martin.schottlender@gmail.com>
2020-03-26 23:23:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-var mysql = require('mysql');
+const mysql = require('mysql');
+const { Sequelize } = require('sequelize');
 const express = require('express');
 const fs = require('fs');
-var path = require('path');
-var httpServer = express();
+const path = require('path');
+const httpServer = express();
+const bodyParser = require('body-parser');
+const productsService = require('./services/products');
+const addressesService = require('./services/addresses');
+const reportsService = require('./services/reports');
+
+httpServer.use(bodyParser.json())
 
 //For settings file
-var settingsFil = fs.readFileSync("./settings.json");
+const settingsFil = fs.readFileSync("./settings.json");
 
 try {
 	var settings = JSON.parse(settingsFil);
@@ -15,61 +22,51 @@ catch(e) {
 	process.exit();
 }
 
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "micontraCOVID19",
-    database: "mydb"
+const sequelize = new Sequelize('mysql://escho:micontraCOVID19:3306/mydb');
+
+httpServer.post('/report', async (req,res) => {
+    const report = await reportsService.createReport({productName: req.query.product_name,
+                                                      productDescription: req.query.product_description,
+                                                      placeName: req.query.place_name,
+                                                      placeDescription: req.query.place_description,
+                                                      fullAddress: req.query.full_address,
+                                                      availability: req.query.availability,
+                                                      comment: req.query.comment});
+    res.status(203).send(report);
 });
-  
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("DB Connected.");
+
+httpServer.get('/report', async (req,res) => {
+    const latestReport = await reportsService.getLatestReport({productName: req.query.product_name,              
+                                                               placeName: req.query.place_name,
+                                                               fullAddress: req.query.full_address});
+    res.status(200).send(latestReport[0].dataValues);
+});
+
+httpServer.get('/productList', async (req,res) => {
+    const products = await productsService.getProducts();
+    res.status(200).send(products);
+});
+
+httpServer.post('/productList', async (req,res) => {
+
+    const product = await productsService.createProduct({name: req.query.name,
+                                                         description: req.query.description});
+    res.status(203).send(product);
+});
+
+httpServer.get('/addressList', async (req,res) => {
+    const addresses = await addressesService.getAddresses();
+    res.status(200).send(addresses);
+});
+
+httpServer.post('/addressList', async (req,res) => {
+    console.log(req.query)
+    const address = await addressesService.createAddress({name: req.query.name,
+                                                         description: req.query.description,
+                                                         fullAddress: req.query.full_address});
+    res.status(203).send(address);
 });
 
 //HTTP Server
 httpServer.use(express.static(settings.http.static));
 httpServer.listen(settings.http.port);
-
-
-httpServer.get('/', (req, res) => {
-    // console.log('HTTP SERVER : sending status to ', req.ip);
-    // console.log(__dirname + '/html/index.html');
-    res.sendFile(__dirname + '/html/index.html');
-});
-
-httpServer.get('/submitData', (req, res) => {
-    console.log('Data received.');
-    var sql = "INSERT INTO customers (product, address, availability) VALUES ('"+ req.query.prod_submit +"', '"+ req.query.addr_submit +"','"+ req.query.avail_submit +"')";
-    con.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("Input sent");
-    });
-    res.sendFile(__dirname + '/html/index.html');
-});
-
-httpServer.get('/searchData', (req, res) => {
-    console.log('Query received.');
-    var sql = "SELECT availability FROM customers WHERE product = '"+ req.query.prod_input +"' AND address = '"+ req.query.addr_input +"';";
-    con.query(sql, function (err, result) {
-        if (err) throw err;
-        if(result[0] === undefined){
-            console.log('No product found');
-        } else {
-            let prodAvailability = result[0].availability;
-            console.log("Query successful. Result = " + prodAvailability);
-            res.send(prodAvailability) 
-        }
-    });
-});
-
-// function main() {
-// console.log('DOM was created, starting connection with API');
-
-// document.addEventListener("DOMContentLoaded", main);
-
-// document.getElementById("enviar").addEventListener("click", function (){
-//     console.log('Hola!');
-//     
-// });
-// }
