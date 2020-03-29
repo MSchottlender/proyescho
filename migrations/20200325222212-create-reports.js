1f3aa1651d98e896279c01f3f4e6fd208a49189e
MSchottlender <martin.schottlender@gmail.com>
2020-03-29 01:36:57 -0300

    Add files via upload

---
+++ migrations/20200325222212-create-reports.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('reports', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      productId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      addressId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      availability: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      comment: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('reports');
+  }
+};
