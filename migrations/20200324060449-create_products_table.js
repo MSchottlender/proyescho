d96dc92eb0948cb896eacd6eb51e1bea818f6dcc
MSchottlender <martin.schottlender@gmail.com>
2020-03-29 01:36:22 -0300

    Create 20200324060449-create_products_table.js

---
+++ migrations/20200324060449-create_products_table.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('products', { id:  {
+                                                            allowNull: false,
+                                                            autoIncrement: true,
+                                                            primaryKey: true,
+                                                            type: Sequelize.INTEGER
+                                                          },
+                                                    name: {type: Sequelize.STRING, allowNull: false},
+                                                    description: Sequelize.STRING,
+                                                    created_at: {
+                                                      allowNull: false,
+                                                      type: Sequelize.DATE
+                                                    },
+                                                    updated_at: {
+                                                      allowNull: false,
+                                                      type: Sequelize.DATE
+                                                    } });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('products');
+  }
+};
